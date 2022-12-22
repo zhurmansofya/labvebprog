e33156770ff17f426f6f243d47a06547368240fc
zhurmansofya <115921134+zhurmansofya@users.noreply.github.com>
2022-12-22 22:21:28 +0300

    Create delete.php

---
+++ lab5/delete.php
@@ -0,0 +1,17 @@
+<?php
+require("db.php");
+
+if (isset($_GET['id'])) {
+   
+    $id = $_GET["id"];
+
+    
+    $db->query("DELETE FROM ownpages WHERE id=$id");
+
+   
+    echo "<script>
+    location.href = 'index.php';
+    </script>";
+
+}
+?>
